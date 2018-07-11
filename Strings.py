4bae05daaa62e47df720fbb0f61b5ca4b9148519
student <you@example.com>
2018-07-11 15:41:28 -0500

    checkin

---
+++ Strings.py
@@ -0,0 +1,27 @@
+import os
+os.system("clear")
+
+
+
+bandMate = "Ringo"
+
+print("the name is this long", len(bandMate))
+print(bandMate.upper())
+print(bandMate.lower())
+print(bandMate[2:])
+print(bandMate[2:3])
+print(bandMate[3:])
+
+person = input('Enter your name: ')
+print('Hello', person)
+
+d = int(input("The entered string is x characters long"))
+print("x = 6")
+
+x = "uppercase"
+print(uppercase.upper())
+
+
+
+
+
