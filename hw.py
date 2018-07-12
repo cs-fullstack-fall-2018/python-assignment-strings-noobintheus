ea4585571a4ff09055f91c96a8f78832f1d790d8
student <you@example.com>
2018-07-12 09:09:15 -0500

    check in

---
+++ hw.py
@@ -0,0 +1,15 @@
+#SyntaxError: EOL while scanning string literal
+
+#input("why no quotes)
+#error message file "answers.py" , line 1
+#input("why no quotes)
+#^
+#SyntaxError: Eol while scanning string literal
+#Error because of forgotten quotation mark
+
+
+x = ["apple", "banana", "carrot"]
+x[0] == "apple"
+x[1] == "banana"
+x[2] == "carrots"
+print(x[0]+'s' + x[1]+'s and' + x[2] + 's')
